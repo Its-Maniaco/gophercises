2c23f4619b1bd5c9311039abbc1b0412e7976fa3
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-03-12 00:16:01 +0530

    implemented MapHandler

--- 2. URL Shortner/handler.go
+++ 2. URL Shortner/handler.go
@@ -12,7 +12,13 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if pathsToUrls[r.URL.Path] == "" {
+			fallback.ServeHTTP(w, r)
+		} else {
+			http.Redirect(w, r, pathsToUrls[r.URL.Path], http.StatusPermanentRedirect)
+		}
+	})
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -23,8 +29,8 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // YAML is expected to be in the format:
 //
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
 //
 // The only errors that can be returned all related to having
 // invalid YAML data.
