ae9a1e9fc37656aaed8977d0075fbada1cd2752c
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-03-12 17:30:35 +0530

    added YAML handler

--- 2. URL Shortner/main/main.go
+++ 2. URL Shortner/main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gophercises/urlshort"
+	urlshort "github.com/Its-Maniaco/gophercises/urlshortner"
 )
 
 func main() {
@@ -16,6 +16,8 @@ func main() {
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	// fmt.Println("Starting the server on :8080")
+	// http.ListenAndServe(":8080", mapHandler)
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
@@ -23,8 +25,9 @@ func main() {
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
+  url: https://github.com/gophercises/urlshort/tree/final
 `
+
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
